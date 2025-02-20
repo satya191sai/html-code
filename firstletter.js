f2c584ce5eddfe187993dadde570dd611cb5de85
satya191sai <sai67038@gmail.com>
2025-02-20 10:14:36 +0530

    df

---
+++ firstletter.js
@@ -0,0 +1,14 @@
+let emp=["satya","kumar","vinay","tarun"]
+console.log(emp);
+console.log(emp.length);
+emp.forEach(emp=>
+{
+   let new_array=(emp[0]);
+   console.log(new_array[0]);
+
+}
+)
+firstCharacters = emp.map(emp => emp.charAt(0));
+console.log(firstCharacters);
+
+
---
+++ fun.js
@@ -0,0 +1,11 @@
+function changeBtnName(){
+    //alert("Test Case 123")
+    let btnRef = document.getElementById('abc')
+    console.log(btnRef)
+    btnRef.innerHTML="apply"
+}
+function currentdate(){
+    //alert("Test Case 123")
+document.getElementById('abc1').style.backgroundColor="red";
+}
+
