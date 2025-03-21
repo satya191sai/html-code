9f1af87e87f57454a1d0bee1536af1eb924fb218
satya1919sai <sai67038@gmail.com>
2025-03-21 12:29:45 +0530

    JLJ

---
+++ name.js
@@ -0,0 +1,7 @@
+function changefocus(){
+document.getElementById('abd').style.backgroundColor="blue";
+}
+function changename(){
+    let name=document.getElementById('cde').value;
+    document.getElementById('cde').value=name.toUpperCase();    
+}
